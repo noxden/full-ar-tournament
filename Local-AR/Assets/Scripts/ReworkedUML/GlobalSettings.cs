cbb40c06faaec7a2cfd990ab1edc6dfa0e7d6d9c
Noxden <Daniel.Heilmann@stud.h-da.de>
2022-07-30 18:21:55 +0200

    Added static GlobalSettings for variable "maxMonstersInBag"

---
+++ Local-AR/Assets/Scripts/ReworkedUML/GlobalSettings.cs
@@ -0,0 +1,16 @@
+//================================================================
+// Darmstadt University of Applied Sciences, Expanded Realities
+// Course:       Local Multiplayer AR (by Jan Alexander)
+// Script by:    Daniel Heilmann (771144)
+// Last changed: 30-06-22
+//================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalSettings
+{
+    //# Public Variables 
+    public static int maxMonstersInBag = 1;     //< Static instead of const for when "variable team size"-lobbies are being implemented later on
+}
