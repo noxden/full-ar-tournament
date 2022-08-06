a7fcf94bd53b45e4033df9055e9fdc6d654ac4dd
Noxden <Daniel.Heilmann@stud.h-da.de>
2022-08-06 21:24:34 +0200

    Implemented AR toggle button in ARTestScene

--- Local-AR/Assets/Scripts/UI/Button_ToggleAR.cs
+++ Local-AR/Assets/Scripts/UI/Button_ToggleAR.cs
@@ -2,40 +2,79 @@
 // Darmstadt University of Applied Sciences, Expanded Realities
 // Course:       Local Multiplayer AR (by Jan Alexander)
 // Script by:    Daniel Heilmann (771144)
-// Last changed: 04-08-22
+// Last changed: 05-08-22
+// TODO: Maybe just solve the background canvas stuffs here?
+// TODO: But that still wont account for the AR Button... I guess that could just be put on a non-CanvasMenu canvas... but then it'll always be visible, also during 
+// TODO: loading and end screen... Unless those have their own backgrounds setup and are rendered on top...
 //================================================================
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using TMPro;
 
 public class Button_ToggleAR : MonoBehaviour
 {
     //# Public Variables 
     public bool isAREnabled;
+    public Camera NonARCamera;
+    public Camera ARCamera;
     public GameObject ARSession;
-    public MenuName nonARMenu;
+    public Canvas nonARBackground;
 
     //# Private Variables
-    
+    private TextMeshProUGUI buttonText;
 
     //# Monobehaviour Events 
     private void Awake()
     {
-        
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     private void Start()
     {
+        UpdateActiveCamera();
+        UpdateButtonText();
     }
 
     //# Public Methods 
 
     //# Private Methods 
+    private void ToggleARMode()
+    {
+        isAREnabled = !isAREnabled;
+        UpdateActiveCamera();
+        UpdateButtonText();
+
+        if (isAREnabled)
+        {
+            Debug.Log($"Button_ToggleAR.ToggleARMode: Turned on AR.");
+        }
+        else
+        {
+            ARCamera.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+            Debug.Log($"Button_ToggleAR.ToggleARMode: Turned off AR.");
+        }
+    }
+
+    private void UpdateActiveCamera()
+    {
+        NonARCamera.gameObject.SetActive(!isAREnabled);
+        ARCamera.gameObject.SetActive(isAREnabled);
+    }
+
+    private void UpdateButtonText()
+    {
+        if (isAREnabled)
+            buttonText.text = "Disable AR";
+        else
+            buttonText.text = "Enable AR";
+    }
 
     //# Input Event Handlers 
     public void OnButtonPressed()
     {
+        ToggleARMode();
     }
 }
