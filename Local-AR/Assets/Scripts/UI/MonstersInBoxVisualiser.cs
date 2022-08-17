172210a3e9fa4e5f51e273bfb3a778ac32ec2497
Noxden <Daniel.Heilmann@stud.h-da.de>
2022-08-17 23:33:41 +0200

    Fixed bug where MonsterToBag buttons were instantiated multiple times
    - This was due to MonstersInBoxVisualiser instantiating one button for each MonsterData in GameManager.MonsterLibrary EVERY TIME UserProfile.MonsterBagStateChanged was called
    - This was an oversight by me. I should have taken out the delegate subscriptions way earlier, as I did not need them anymore for my current implementation of MonstersInBoxVisualiser

--- Local-AR/Assets/Scripts/UI/MonstersInBoxVisualiser.cs
+++ Local-AR/Assets/Scripts/UI/MonstersInBoxVisualiser.cs
@@ -17,8 +17,8 @@ public class MonstersInBoxVisualiser : MonoBehaviour
     //# Private Variables 
 
     //# Monobehaviour Events 
-    private void OnEnable() => UserProfile.MonsterBagStateChanged += UpdateButtonList;
-    private void OnDisable() => UserProfile.MonsterBagStateChanged += UpdateButtonList;
+    // private void OnEnable() => UserProfile.MonsterBagStateChanged += UpdateButtonList;   //< Deprecated, as the Buttons are now based on the MonsterData from MonsterLibrary.
+    // private void OnDisable() => UserProfile.MonsterBagStateChanged += UpdateButtonList;
 
     private void Start()
     {
